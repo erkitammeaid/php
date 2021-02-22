aae8c6f5e23b85ff6e74b712606b3bbf696e8414
erkitammeaid <erkipoiss01@gmail.com>
2021-02-22 12:19:56 +0200

    ssss

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+class Task {
+
+    protected $description;
+
+    protected $completed = false;
+
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
+
+
+    public function isComplete()
+    {
+        return $this->completed;
+    }
+}
+
+$task = new Task('Go to the store');
+
+var_dump($task->isComplete());
+
+// require 'index.view.php';
